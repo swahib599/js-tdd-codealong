40978f5320cf2bafc75eb115302db529aead7f5b
Shaib Echesa Makokha <echesaswahib@gmail.com>
2024-08-20 01:05:29 +0300

    compares ageOfPerson to expectedAge

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -3,7 +3,9 @@ import { currentAgeForBirthYear } from "../utils";
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
     const birthYear = 1984;
+    const currentYear = new Date().getFullYear();
+    const expectedAge = currentYear - birthYear;
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    expect(ageOfPerson).toBe(expectedAge); // This line checks against the dynamically calculated expectedAge
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
 // Your code here
+
+
+export function currentAgeForBirthYear(birthYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+  }
